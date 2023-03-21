fb25d8f0ae68f21454f205089e2fb45003bf8706
Dileep19977 <119456748+Dileep19977@users.noreply.github.com>
2023-03-21 17:33:29 -0500

    React Project Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
 import Form from './components/Form.js';
-import MainPage from './components/MainPage.js';
+import AppPage from './components/MainPage.js';
 export default function App() {
   return (
     <div>
-      <MainPage></MainPage>
+      <AppPage></AppPage>
     </div>
   );
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Form from './Form.js';
+import AppPage from './MainPage.js';
 import Products from './Products.js';
 
-const MainPage = () => {
+
+const AppPage = () => {
+
+  const [details, setdetails] = useState(false);
+const [products, setproducts] = useState(false);
+
   const NavLOD = () => {
-    return <Form></Form>;
+    console.log(' Deals Click works');
+    setdetails(true)
+    setproducts(false)
   };
 
   const NavLOP = () => {
-    return <Products></Products>;
+    console.log(' Products Click works');
+    setdetails(false)
+    setproducts(true)
   };
 
   return (
     <body>
-      <h1>Click Below Link to View the List of Products</h1>
+      <h4>Click Below Link to View the List of Products</h4>
       <div onClick={NavLOP}>List of Products</div>
 
-      <h1>Click Below Link to View the List of Deals</h1>
+      <h4>Click Below Link to View the List of Deals</h4>
       <div onClick={NavLOD}>List of Deals</div>
+      <br></br>
+      <hr></hr>
+      {details ? <Form></Form> : <></>}
+      {products ? <Products></Products> : <></>}
     </body>
   );
 };
 
-export default MainPage;
+export default AppPage;
