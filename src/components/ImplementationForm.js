6c9bd6215f3b95c888265a00f0b4d717797e0229
Dileep19977 <119456748+Dileep19977@users.noreply.github.com>
2023-03-24 10:44:40 -0500

    Form validations

--- src/components/ImplementationForm.js
+++ src/components/ImplementationForm.js
@@ -1,32 +1,70 @@
 import React, { useState, useEffect } from 'react';
 
 function LoginForm() {
-  const [username, setUsername] = useState('Dileep Kumar Reddy Bodanam');
-  const [Preferedname, setPreferedname] = useState('Dileep Kumar Reddy B');
-  const [gender, setGender] = useState('Male');
-  const [address, setAddress] = useState('Irving, Dallas');
+  const [username, setUsername] = useState('');
+  const [Preferedname, setPreferedname] = useState('');
+  const [gender, setGender] = useState('');
+  const [address, setAddress] = useState('');
   const [array, setArray] = useState([]);
-  const [setsuccess, settsuccess] = useState();
+  const [setsuccess, settsuccess] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+    if (event.target.value === '') {
+      alert('Please enter a valid name');
+    }
+  };
+
+  const handlePreferednameChange = (event) => {
+    setPreferedname(event.target.value);
+    if (event.target.value === '') {
+      alert('Please enter a preferred name');
+    }
+  };
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+    if (event.target.value === '') {
+      alert('Please select a gender');
+    }
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+    if (event.target.value === '') {
+      alert('Please enter an address');
+    }
+  };
 
   const getval = (event) => {
     event.preventDefault();
-    console.log('username:', username);
-    console.log('Prefered Name:', Preferedname);
-    console.log('gender:', gender);
-    console.log('address:', address);
-    setArray([
-      {
-        Username: username,
-        Preferedname: Preferedname,
-        Gender: gender,
-        Address: address,
-      },
-    ]);
-    if (array) {
+    let valid = true;
+
+    // Check if all fields are filled
+    if (username === '' || Preferedname === '' || gender === '' || address === '') {
+      alert('Please fill in all fields');
+      valid = false;
+    }
+
+    // Submit data if all fields are valid
+    if (valid) {
+      setArray([
+        {
+          Username: username,
+          Preferedname: Preferedname,
+          Gender: gender,
+          Address: address,
+        },
+      ]);
       settsuccess('Successfully Submitted..!!!');
       console.log('Successfully submitted..!!');
+      console.log('username:', username);
+      console.log('Prefered Name:', Preferedname);
+      console.log('gender:', gender);
+      console.log('address:', address);
     }
   };
+
   const refresh = () => {
     console.log('Page Refreshed');
   };
@@ -40,35 +78,24 @@ function LoginForm() {
       </h2>
       <div>
         <label>Name : </label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        ></input>
+        <input type="text" value={username} onChange={handleUsernameChange}></input>
       </div>
       <div>
         <label>Prefered Name : </label>
-        <input
-          type="text"
-          value={Preferedname}
-          onChange={(e) => setPreferedname(e.target.value)}
-        ></input>
+        <input type="text" value={Preferedname} onChange={handlePreferednameChange}></input>
       </div>
       <div>
         <label>Gender : </label>
-        <input
-          type="text"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        ></input>
+        <select value={gender} onChange={handleGenderChange}>
+          <option value="">Select Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
       </div>
       <div>
         <label>Address : </label>
-        <input
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        ></input>
+        <input type="text" value={address} onChange={handleAddressChange}></input>
       </div>
       <br></br>
       <div>
